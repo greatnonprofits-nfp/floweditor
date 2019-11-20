55eb9700bb445d62f039c75cde4b9db72538ee23
kaiobrito <kaio.a7x@gmail.com>
2019-11-20 19:34:59 -0300

    Fix issue on rendering lookup selected value

--- src/components/flow/actions/calllookup/CallLookup.tsx
+++ src/components/flow/actions/calllookup/CallLookup.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { CallLookup } from 'flowTypes';
 
 const CallLookupComp: React.SFC<CallLookup> = ({ lookup_db }): JSX.Element => (
-  <div>{lookup_db}</div>
+  <div>{lookup_db.text}</div>
 );
 
 export default CallLookupComp;
