5fb66347a0803bd7829e81c24f3d4f8a80e3fec1
kaiobrito <kaio.a7x@gmail.com>
2019-11-27 22:48:51 -0300

    Update lookup lambda

--- lambda/lookups.js
+++ lambda/lookups.js
@@ -2,7 +2,18 @@ const lookups = [
   { id: 'my-first-lookup', text: 'My First Lookup' },
   { id: 'my-other-lookup', text: 'My Other Lookup' }
 ];
+
+const lookupParameters = [
+  { text: 'id', type: 'String', id: 'id' },
+  { text: 'objectId', type: 'String', id: 'objectId' },
+  { text: 'phone', type: 'String', id: 'phone' }
+];
+
 const { getOpts } = require('./utils');
 
-exports.handler = (evt, ctx, cb) =>
-  cb(null, getOpts({ body: JSON.stringify({ results: lookups }) }));
+exports.handler = (evt, ctx, cb) => {
+  if (evt.queryStringParameters.db) {
+    return cb(null, getOpts({ body: JSON.stringify({ results: lookupParameters }) }));
+  }
+  return cb(null, getOpts({ body: JSON.stringify({ results: lookups }) }));
+};
