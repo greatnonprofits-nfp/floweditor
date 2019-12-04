cde866a40f8805df600ebea362976fe088168dd9
kaiobrito <kaio.a7x@gmail.com>
2019-12-03 21:20:54 -0300

    Fix shorten url typing

--- src/components/flow/routers/shortenurl/helpers.tsx
+++ src/components/flow/routers/shortenurl/helpers.tsx
@@ -1,6 +1,12 @@
-import { FormEntry, FormState, StringEntry, NodeEditorSettings } from 'store/nodeEditor';
+import {
+  FormEntry,
+  FormState,
+  StringEntry,
+  NodeEditorSettings,
+  AssetEntry
+} from 'store/nodeEditor';
 import { TrackableLinkType, TrackableLinkAction } from 'flowTypes';
-import { RenderNode } from 'store/flowContext';
+import { RenderNode, AssetType } from 'store/flowContext';
 import { Types } from 'config/interfaces';
 import { getActionUUID } from 'components/flow/actions/helpers';
 import { createWebhookBasedNode } from '../helpers';
@@ -10,7 +16,7 @@ export interface ShortenUrlEntry extends FormEntry {
 }
 
 export interface ShortenUrlFormState extends FormState {
-  shortenUrl: ShortenUrlEntry;
+  shortenUrl: AssetEntry;
   resultName: StringEntry;
 }
 
@@ -19,13 +25,19 @@ export const nodeToState = (settings: NodeEditorSettings): ShortenUrlFormState =
     const action = settings.originalAction as TrackableLinkAction;
     return {
       resultName: { value: action.result_name },
-      shortenUrl: { value: { id: action.shorten_url.id, text: action.shorten_url.text } },
+      shortenUrl: {
+        value: {
+          id: action.shorten_url.id,
+          name: action.shorten_url.text,
+          type: AssetType.TrackableLink
+        }
+      },
       valid: true
     };
   }
   return {
     resultName: { value: 'Result' },
-    shortenUrl: { value: { id: '', text: '' } },
+    shortenUrl: { value: { id: '', name: '', type: AssetType.TrackableLink } },
     valid: false
   };
 };
@@ -34,9 +46,13 @@ export const stateToNode = (
   settings: NodeEditorSettings,
   state: ShortenUrlFormState
 ): RenderNode => {
+  console.log(state.shortenUrl);
   const action: TrackableLinkAction = {
     result_name: state.resultName.value,
-    shorten_url: state.shortenUrl.value,
+    shorten_url: {
+      id: state.shortenUrl.value.id,
+      text: state.shortenUrl.value.name
+    },
     type: Types.shorten_url,
     uuid: getActionUUID(settings, Types.shorten_url)
   };
