6d67247a8e117d97a287bd8c246023ba5394a257
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2019-12-19 02:48:10 +0100

    Added timeout of 30 mins in the new flow editor

--- src/components/form/timeout/TimeoutControl.tsx
+++ src/components/form/timeout/TimeoutControl.tsx
@@ -19,6 +19,7 @@ export const TIMEOUT_OPTIONS = [
   { value: 300, label: '5 minutes' },
   { value: 600, label: '10 minutes' },
   { value: 900, label: '15 minutes' },
+  { value: 1800, label: '30 minutes' },
   { value: 3600, label: '1 hours' },
   { value: 7200, label: '2 hours' },
   { value: 10800, label: '3 hours' },
