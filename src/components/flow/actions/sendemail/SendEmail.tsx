00b35d64c63ec7bf7dc99598de1302d85c552d7f
Teeh Amaral <contato@teehamaral.me>
2020-05-05 16:42:58 -0300

    remove ? from sendemail subject

--- src/components/flow/actions/sendemail/SendEmail.tsx
+++ src/components/flow/actions/sendemail/SendEmail.tsx
@@ -5,7 +5,7 @@ import styles from './SendEmail.module.scss';
 const SendEmailComp: React.SFC<SendEmail> = (action: SendEmail): JSX.Element => {
   return (
     <>
-      <div>{action.subject}?</div>
+      <div>{action.subject}</div>
       <div>
         {action.attachments && action.attachments.length > 0 ? (
           <div className={`${styles.attachment} fe-paperclip`} />
