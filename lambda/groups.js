487269b6642b14e136e47800d40b448268e8835c
Ihor Palii <ithor@communityconnectlabs.com>
2021-02-17 15:30:57 +0200

    Removed variable support on add contact group flow step.

--- lambda/groups.js
+++ lambda/groups.js
@@ -13,6 +13,23 @@ exports.handler = (request, context, callback) => {
       count: 0
     });
   } else {
-    respond(callback, { results: [] });
+    respond(callback, {
+      results: [
+        {
+          uuid: '8498391d-8086-4818-b2db-08cfd72b8008',
+          name: 'Test Group 1',
+          query: null,
+          status: 'ready',
+          count: 1
+        },
+        {
+          uuid: '8498391d-8086-4818-b2db-08cfd72b8009',
+          name: 'Test Group 2',
+          query: null,
+          status: 'ready',
+          count: 1
+        }
+      ]
+    });
   }
 };
